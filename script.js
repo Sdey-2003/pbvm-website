273719dda3973f864c3c75cc193e528a916d5ce9
Sdey-2003 <sdey46603@gmail.com>
2025-06-13 03:19:03 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-document.getElementById("registrationLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
+//document.getElementById("registrationLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
 //document.getElementById("admitCardLink").href = "https://example.com/admit";
 //document.getElementById("resultLink").href = "https://example.com/result";
