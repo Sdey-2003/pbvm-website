b9083f35833f4b1d5078fff61e6b5ab0a94074cb
Sdey-2003 <sdey46603@gmail.com>
2025-06-13 03:30:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("registrationLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
-document.getElementById("admitCardLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
-document.getElementById("resultLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
+//document.getElementById("admitCardLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
+//document.getElementById("resultLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
