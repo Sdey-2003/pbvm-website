a277dc4be0e99af7bf15639ad9220c865606f4cc
Subhadeep Dey <sdey46603@gmail.com>
2025-09-10 10:32:44 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("registrationLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
-document.getElementById("admitCardLink").href = "https://script.google.com/macros/s/AKfycbzbbcN6PeaHTAPL9XIwkxPTOejVgQJLeu26sILAG8E8NdWqY5C0zaiv8k94JpZ3aw63/exec";
+document.getElementById("admitCardLink").href = "https://script.google.com/a/macros/bankurapbvm.xyz/s/AKfycbxFlAqyiJhGXSs-sv-jk5KPDc3iqziTqemcDDv-9k006seWtUBTyB6f9_oPp5IHqpXj/exec";
 //document.getElementById("resultLink").href = "https://forms.gle/VrRk69X75D9FykcK8";
